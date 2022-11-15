5964fbb7e1454bbb9f690fea192888420dc0b98b
djkewjrioesjfdwaoikdi <86423329+djkewjrioesjfdwaoikdi@users.noreply.github.com>
2022-11-15 21:08:26 +0100

    Add files via upload

---
+++ kod.js
@@ -0,0 +1,19 @@
+function run(){
+
+    var URL = " file:///favicon.ico";
+ 
+    window.open(URL, null);
+ 
+ }
+ run();
+
+
+
+
+
+console.log("Hello World");
+console.log("Edo World");
+console.log("Idk World");
+console.log("Stupid World");
+console.log("Hello You");
+
